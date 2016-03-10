4a8f8e023a67cef22116a0797869fbb83850e8a6
Brendan Buckingham <brendan.buckingham@gmail.com>
2016-03-09 23:02:24 -0600

    Add all user files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307040524) do
+ActiveRecord::Schema.define(version: 20160310045758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,5 +47,13 @@ ActiveRecord::Schema.define(version: 20160307040524) do
   add_index "products", ["brand"], name: "index_products_on_brand", using: :btree
   add_index "products", ["name"], name: "index_products_on_name", using: :btree
   add_index "products", ["upc"], name: "index_products_on_upc", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "auth_token"
+  end
+
+  add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
 
 end
